3de2d2c4e6a9190fd77384b30cd1f30276bdc2b9
oprogramador <uomodislesia@gmail.com>
2024-04-20 05:34:32 +0200

    chore: measure coverage

---
+++ nyc.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  lines: 100,
+  functions: 100,
+  branches: 100,
+  statements: 100,
+  watermarks: {
+    lines: [90, 100],
+    functions: [90, 100],
+    branches: [90, 100],
+    statements: [90, 100],
+  },
+};
